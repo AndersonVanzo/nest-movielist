35de31d143b0fb3df0d754d0751928327ecf618f
AndersonVanzo <anderson.vanzo01@gmail.com>
2023-06-13 22:42:26 -0300

    feat: create movie

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movie } from './entities/movie.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MoviesService {
+  constructor(
+    @InjectRepository(Movie) private movieRepository: Repository<Movie>,
+  ) {}
   create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+    return this.movieRepository.create(createMovieDto);
   }
 
   findAll() {
