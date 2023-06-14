f197067e4c31ce63a7d28d42f1e54b486a2b6f47
AndersonVanzo <anderson.vanzo01@gmail.com>
2023-06-13 22:08:30 -0300

    feat: movies entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,16 @@
-export class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  description: string;
+}
