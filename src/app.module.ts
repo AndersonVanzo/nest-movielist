7d2725bce6b33b6ff4f56b3eb8174727fbe5c011
AndersonVanzo <anderson.vanzo01@gmail.com>
2023-06-13 22:08:05 -0300

    feat: typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './ormconfig';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
